df333171f093891236536eb0fda40ba14f168e32
downn_falls <maneeohooho3790@gmail.com>
2025-03-16 20:07:57 +0700

    deleted unneccessary file

--- testcode/map.py
+++
@@ -1,9 +0,0 @@
-def myfunc(a, b):
-  return a + b
-
-x = map(myfunc, ('apple', 'banana', 'cherry'), ('orange', 'lemon', 'pineapple'))
-
-print(x) #address map
-
-#convert the map into a list, for readability:
-print(list(x)) # value in map
